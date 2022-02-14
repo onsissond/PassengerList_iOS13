628c6c7b46778f21f04225b644d0ff8597b8b5db
Evgeny <sukhanovevgeniy96@gmail.com>
2022-02-14 16:46:37 +0300

    Optimise shadow rendering

--- PassengersFramework/PassengersFramework/Modules/PassengersList/UI/ReusableView/PassengerSectionBackgroundView.swift
+++ PassengersFramework/PassengersFramework/Modules/PassengersList/UI/ReusableView/PassengerSectionBackgroundView.swift
@@ -5,26 +5,21 @@
 import UIKit
 
 final class PassengerSectionBackgroundView: UICollectionReusableView {
-
-    private var _view: UIView = {
-        let view = UIView()
-        view.backgroundColor = Asset.backgroundSecondary.color
-        view.layer.cornerRadius = 12
-        view.layer.shadowPath = UIBezierPath(
-            roundedRect: view.bounds,
-            cornerRadius: 12
-        ).cgPath
-        view.layer.shadowColor = UIColor(
+    private var _shapeLayer: CAShapeLayer = {
+        $0.fillColor = Asset.backgroundSecondary.color.cgColor
+        $0.shadowColor = UIColor(
             red: 0.292,
             green: 0.414,
             blue: 0.65,
             alpha: 0.16
         ).cgColor
-        view.layer.shadowOpacity = 1
-        view.layer.shadowRadius = 16
-        view.layer.shadowOffset = CGSize(width: 0, height: 4)
-        return view
-    }()
+        $0.shadowOffset = CGSize(width: 0, height: 4)
+        $0.shadowOpacity = 1
+        $0.shadowRadius = 12
+        return $0
+    }(CAShapeLayer())
+
+    private var _view = UIView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -32,11 +27,21 @@ final class PassengerSectionBackgroundView: UICollectionReusableView {
         _setupSubviews()
     }
 
-    private func _setupSubviews() {
-        addSubview(_view, constraints: .allAnchors)
-    }
-
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        _shapeLayer.path = UIBezierPath(
+            roundedRect: _view.bounds,
+            cornerRadius: _shapeLayer.shadowRadius
+        ).cgPath
+        _shapeLayer.shadowPath = _shapeLayer.path
+    }
+
+    private func _setupSubviews() {
+        addSubview(_view, constraints: .allAnchors)
+        _view.layer.addSublayer(_shapeLayer)
     }
 }
